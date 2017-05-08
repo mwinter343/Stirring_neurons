2b146e27604b64ef8455bfad0e04ade17cf57d45
Michael <mwinter.343@gmail.com>
2017-05-08 05:01:03 -1000

    Adding comment controller

---
+++ frontend/controllers/CommentController.php
@@ -0,0 +1,100 @@
+<?php
+namespace frontend\controllers;
+
+use Yii;
+use yii\base\InvalidParamException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use common\models\LoginForm;
+use frontend\models\PasswordResetRequestForm;
+use frontend\models\ResetPasswordForm;
+use frontend\models\SignupForm;
+use frontend\models\ContactForm;
+use yii\web\UrlManager;
+
+/**
+ * Comment controller
+ */
+class CommentController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout', 'signup'],
+                'rules' => [
+                    [
+                        'actions' => ['signup'],
+                        'allow' => true,
+                        'roles' => ['?'],
+                    ],
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     *  Renders Comments
+     *
+     * defaults to top 20 Comments
+     */
+    public function renderComments(){
+    }
+
+    /**
+     *  Create Comments
+     */
+    public function createComments(){
+        //if users are logged in display name
+        //otherwise allow comment via anonymous names
+    }
+
+    /**
+     *  Archives Comments
+     */
+    public function deleteComments(){
+
+    }
+
+    /**
+     *  Edit Comments
+     */
+    public function editComments(){
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+
+}
