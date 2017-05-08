890181e0793307abb4b1cde5af0245bf87d347c3
michael <mwinter.343@gmail.com>
2017-05-08 08:06:09 -1000

    added Crud for comments table

---
+++ frontend/models/CommentsSearch.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use frontend\models\Comments;
+
+/**
+ * CommentsSearch represents the model behind the search form about `frontend\models\Comments`.
+ */
+class CommentsSearch extends Comments
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['idComment', 'idUser'], 'integer'],
+            [['comment', 'Created', 'Archived'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Comments::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'idComment' => $this->idComment,
+            'idUser' => $this->idUser,
+            'Created' => $this->Created,
+            'Archived' => $this->Archived,
+        ]);
+
+        $query->andFilterWhere(['like', 'comment', $this->comment]);
+
+        return $dataProvider;
+    }
+}
