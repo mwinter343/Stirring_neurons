76f14a84342e8bbf8328aa9243394a52ee1eef3b
Michael <mwinter.343@gmail.com>
2017-06-07 18:08:25 -1000

    helper methods added

--- frontend/views/comment/view.php
+++ frontend/views/comment/view.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\widgets\DetailView;
 use frontend\models\Comments;
+use frontend\components\Helper;
 
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Comments */
@@ -15,32 +16,13 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title)?></h1>
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->idComment], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->idComment], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'idComment',
-            'idUser',
-            'comment',
-            'Created',
-            'Archived',
-        ],
-    ]) ?>
-
     <?php
         $comment = new Comments();
 
-        echo "" . $comment->showComments(2);
+        $helper = new frontend\components\Helper();
+
+        $helper->createdTableHyperLinks();
+
     ?>
 
 </div>
